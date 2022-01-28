acc09822d3b6c29a4cf27c5d0b58e9a182bc6a90
vizeit <jigparekh@gmail.com>
2022-01-27 21:33:20 -0800

    module

--- GraphQLAPI/__init__.py
+++ GraphQLAPI/__init__.py
@@ -1,23 +1,12 @@
 import azure.functions as func
 from azure.functions import AsgiMiddleware
 
-from graphene import ObjectType, String, Schema
+from graphene import Schema
 from starlette_graphene3 import GraphQLApp, make_graphiql_handler
-
-class Query(ObjectType):
-    
-    hello = String(name=String(default_value="stranger"))
-    goodbye = String()
-
-    def resolve_hello(root, info, name):
-        return f'Hello {name}!'
-
-    def resolve_goodbye(root, info):
-        return 'See ya!'
+from .Schemas.Queries import Query
 
 schema = Schema(query=Query)
-#app = GraphQLApp(schema, on_get=make_graphiql_handler())
-app = GraphQLApp(schema)
+app = GraphQLApp(schema, on_get=make_graphiql_handler())
 
 def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
     return AsgiMiddleware(app).handle(req, context)
